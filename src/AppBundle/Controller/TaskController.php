522c62cf9861ff5016868a2ec027561afcebb1c4
Waller Jhonatan <waller@ufv.br>
2017-07-05 16:38:51 -0300

    Cria rota show

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -2,20 +2,53 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class TaskController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/tasks/show", name="showTask")
      */
-    public function indexAction(Request $request)
+    public function showAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render(':task:show.html.twig');
+    }
+
+    /**
+     * @Route("/tasks/{name}", name="homepage")
+     */
+    public function indexAction($name)
+    {
+
+        $tasks = [
+            'andar',
+            'sair',
+            'chegar'
+        ];
+
+        return $this->render('task/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'name' => $name,
+            'tasks' => $tasks
         ]);
+    }
+
+    /**
+     * @Route("/tasks", name="tasks")
+     * @Method("GET")
+     */
+    public function apiAction()
+    {
+        $tasks = [
+            'andar',
+            'sair',
+            'chegar'
+        ];
+
+        return new JsonResponse($tasks);
     }
 }
