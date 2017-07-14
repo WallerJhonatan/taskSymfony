47de9284e698ae30b27adb3f5d057311ea951073
Waller Jhonatan <waller@ufv.br>
2017-07-14 17:50:41 -0300

    adiciona form no login

--- src/Waller/AppBundle/Controller/LoginController.php
+++ src/Waller/AppBundle/Controller/LoginController.php
@@ -4,14 +4,37 @@ namespace Waller\AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Templating\Storage\Storage;
+use Waller\AppBundle\Entity\User;
+use Waller\AppBundle\Form\UserLogin;
+use Symfony\Component\HttpFoundation\Request;
 
 class LoginController extends Controller
 {
     /**
      * @Route("/login", name="login")
      */
-    public function index(){
-        dump('teste');
-        return $this->render(':Login:index.html.twig');
+    public function loginAction(Request $request){
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserLogin::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+
+        return $this->render(
+            ':Login:index.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+//    public function autenticaUsuario(string $token){
+//        Storage::setItem('token', $token);
+//    }
+//
+    public function ajaxAction(Request $request)
+    {
+        $data = $request->request->post('request');
+        dump($data);die;
     }
 }
