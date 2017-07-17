5c91036576d832bf715bdefd7c27f8a3caeeb75c
Waller Jhonatan <waller@ufv.br>
2017-07-17 17:53:47 -0300

    FORM LOGIN

---
+++ src/Waller/AppBundle/Form/UserLogin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Waller\AppBundle\Form;
+
+use Waller\AppBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserLogin extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+        ));
+    }
+}
